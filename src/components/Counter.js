ef8dd6da71c31e9afeba855508da59c83764ccde
maryna.kravchuk <marina.kravchuk.it@gmail.com>
2023-07-30 18:08:04 +0300

    first

---
+++ src/components/Counter.js
@@ -0,0 +1,86 @@
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/counter-slice";
+// import { Component } from "react";
+// import { connect } from "react-redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatchFunction = useDispatch();
+  const counter = useSelector((state) => state.counter.value);
+  const isCounterInvisible = useSelector(
+    (state) => state.counter.isCounterInvisible
+  );
+
+  const incrementHandler = () => {
+    dispatchFunction(counterActions.increment());
+  };
+
+  const decrementHandler = () => {
+    dispatchFunction(counterActions.decrement());
+  };
+
+  const increaseHandler = () => {
+    dispatchFunction(counterActions.increase(100));
+  };
+
+  const toggleCounterHandler = () => {
+    dispatchFunction(counterActions.setCounterVisibility());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Счётчик</h1>
+      {!isCounterInvisible && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={increaseHandler}>+100</button>
+        <button onClick={decrementHandler}>-</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Счётчик</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>+</button>
+//           <button onClick={this.decrementHandler.bind(this)}>-</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Спрятать / Показать</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { userAuthActions } from "../store/user-auth-slice";
+
+const Header = () => {
+  const dispatchFunction = useDispatch();
+  const isUserLoggedIn = useSelector((state) => state.auth.isUserLoggedIn);
+
+  const signOutHandler = () => {
+    dispatchFunction(userAuthActions.signOut());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Главная</a>
+          </li>
+          {isUserLoggedIn && (
+            <li>
+              <a href="/">Мои продажи</a>
+            </li>
+          )}
+          <li>
+            <a href="/">О нас</a>
+          </li>
+          <li>
+            <a href="/">Контакты</a>
+          </li>
+          <li>
+            {isUserLoggedIn && <button onClick={signOutHandler}>Выйти</button>}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
